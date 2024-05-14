31f64f4e78b623997b6f78e7c0156b025089150d
r0xd4n3t <125836836+r0xd4n3t@users.noreply.github.com>
2024-05-14 11:42:55 +0800

    Improve and refactor update script for better readability and error handling
    - Removed unnecessary use of sudo within functions
    - Improved code readability with proper indentation and spacing
    - Enhanced script flow and maintainability

--- pi_hole_updater.sh
+++ pi_hole_updater.sh
@@ -2,21 +2,21 @@
 
 # Check for root privileges
 if [[ $(id -u) -ne 0 ]]; then
-  echo "Please run this script as root (with sudo)." >&2
-  exit 1
+    echo "Please run this script as root (with sudo)." >&2
+    exit 1
 fi
 
 # Function to log messages
 log() {
-  local timestamp=$(date +'%Y-%m-%d %H:%M:%S')
-  echo "$timestamp - $1"
+    local timestamp=$(date +'%Y-%m-%d %H:%M:%S')
+    echo "$timestamp - $1"
 }
 
 # Function to handle errors and exit
 handle_error() {
-  local exit_code="$?"
-  log "Error occurred with exit code $exit_code."
-  exit "$exit_code"
+    local exit_code="$?"
+    log "Error occurred with exit code $exit_code."
+    exit "$exit_code"
 }
 
 # Trap errors and exit
@@ -24,34 +24,35 @@ trap 'handle_error' ERR
 
 # Update system packages
 update_packages() {
-  log "Updating system packages..."
-  sudo apt-get update --fix-missing
-  sudo apt-get -y upgrade
-  sudo apt-get -y autoremove
-  sudo apt-get -y autoclean
+    log "Updating system packages..."
+    apt-get update --fix-missing
+    apt-get -y upgrade
+    apt-get -y autoremove
+    apt-get -y autoclean
 }
 
 # Update Pi-Hole
 update_pihole() {
-  log "Updating Pi-Hole..."
-  sudo pihole -up
-  log "Updating Pi-Hole Gravity..."
-  pihole -g
+    log "Updating Pi-Hole..."
+    pihole -up
+    log "Updating Pi-Hole Gravity..."
+    pihole -g
 }
 
 # Reboot the system
 reboot_system() {
-  log "Rebooting..."
-  sudo systemctl reboot -i
+    log "Rebooting..."
+    systemctl reboot -i
 }
 
 # Main execution
 main() {
-  update_packages
-  update_pihole
-  reboot_system
-  log "Script execution completed successfully."
+    update_packages
+    update_pihole
+    log "Script execution completed successfully."
+    reboot_system
 }
 
 # Run the main function and log the output
 main >> logs.log 2>&1
+
